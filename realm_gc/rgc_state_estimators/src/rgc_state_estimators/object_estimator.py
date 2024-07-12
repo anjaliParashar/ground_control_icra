68bf94ab00d069ee4bc2d9bba8f024d1cfae8299
Joseph Zhang <jzha@mit.edu>
2024-07-12 17:57:05 -0400

    object_estimator

---
+++ realm_gc/rgc_state_estimators/src/rgc_state_estimators/object_estimator.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""Define a state estimator for general objects tracked by vicon"""
+import rospy
+from std_msgs.msg import Empty
+from geometry_msgs.msg import TransformStamped
+
+from rgc_state_estimators.state_estimator import StateEstimator
+
+
+class ObjectStateEstimator(StateEstimator):
+    """
+    State estimator for general objects tracked by vicon
+    """
+
+    def __init__(self):
+        # we need to use a different name for the node
+        # super(ObjectStateEstimator, self).__init__()
+
+        self.user_id = rospy.get_param("~user_id")
+
+        # Initialize the ROS node
+        rospy.init_node(f"{self.user_id}_estimation_node")
+
+        # Fetch parameters
+        self.rate = rospy.get_param("~rate", 10)  # Hz
+        self.dt = 1.0 / self.rate
+        self.rate_timer = rospy.Rate(self.rate)
+
+        # Publisher - to be defined in subclasses, as needed
+        # e.g., self.pub = rospy.Publisher(
+        #           f"{rospy.get_name()}/estimate",
+        #           PoseStamped,
+        #           queue_size=10
+        #       )
+        self.estimate_pub = rospy.Publisher(
+            f"{rospy.get_name()}/estimate", TransformStamped, queue_size=10
+        )
+
+        # Reset event subscriber
+        self.reset_sub = rospy.Subscriber(
+            f"{rospy.get_name()}/reset", Empty, self.reset_state
+        )
+
+        # Other subscribers - to be defined in subclasses, as needed
+        # e.g., rospy.Subscriber("input_topic", MessageType, self.callback_method)
+        self.position_sub = rospy.Subscriber(
+            self.position_topic, TransformStamped, self.position_callback
+        )
+
+        # Initialize the state
+        self.reset_state()
+
+        # Fetch additional parameters
+        self.position_topic = rospy.get_param(
+            "~position_topic", f"/vicon/{self.user_id}/{self.user_id}"
+        )
+        
+    def reset_state(self, msg=None):
+        """Reset the state of the estimator."""
+        return
+
+    def update(self):
+        """Update the filter state and publish the new state estimate."""
+        # no filter currently?
+        self.estimate_pub.publish(self.position_msg)
+        return
+    
+    def position_callback(self, msg):
+        """
+        Update the state based on new position measurements.
+        Placeholder function - implement measurement update logic here.
+        """
+        self.position_msg = msg
+
+
+if __name__ == "__main__":
+    try:
+        ose_node = ObjectStateEstimator()
+        ose_node.run()
+    except rospy.ROSInterruptException:
+        pass
+
